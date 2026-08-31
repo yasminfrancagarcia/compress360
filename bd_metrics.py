6cda1672144e4abd49d5b9add0ec69e3c08514d6
yasminfrancagarcia <yasminfrancagarcia@gmail.com>
2026-08-31 18:47:24 -0300

    rd curves atualizadas

--- bd_metrics.py
+++ bd_metrics.py
@@ -56,6 +56,22 @@ lalic_30k_planas = {
     ])
 }
 
+# 4. LALIC 30k 360 + W-MSE + random crop 256 256 
+
+lalic_360_wmse_Randomcrop = {
+    "bpp": [
+      0.166709,
+      0.338719,
+      1.06543
+    ],
+    "ws_psnr": [
+      28.581836,
+      30.90475,
+      36.497981
+
+    ]
+}
+
 
 # ------------------------------------------------------------
 # 4. LALIC 30k 360 + W-MSE + CropWidth
@@ -98,53 +114,187 @@ def calcular_bd(nome_ref, ref, nome_test, test):
         method="pchip"
     )
 
-    print("=" * 70)
+    print("=" * 5)
     print(f"Referência: {nome_ref}")
     print(f"Comparado:  {nome_test}")
-    print("-" * 70)
+    print("=" * 5)
     print(f"BD-Rate:    {bd_rate:.2f}%")
     print(f"BD-WS-PSNR: {bd_wspsnr:.4f} dB")
-    print("=" * 70)
+    print("=" * 5)
 
     return bd_rate, bd_wspsnr
 
+# w-mse + 360 + random crop 256 256 vs lalic original 
+
+# calcular_bd(
+#     "LALIC original",
+#     lalic_original,
+#     "LALIC 30k 360 + W-MSE + RandomCrop",
+#     lalic_360_wmse_Randomcrop
+# )
+
+# # w-mse + 360 + random crop 256 256 vs lalic 360 + mse
+
+# calcular_bd(
+#     "LALIC 30k 360 + MSE",
+#     lalic_360_mse,
+#     "LALIC 30k 360 + W-MSE + RandomCrop",
+#     lalic_360_wmse_Randomcrop
+# )
 
 
-# 360 + MSE vs W-MSE + CropWidth
+# # 360 + MSE vs W-MSE + CropWidth
 
-calcular_bd(
-    "LALIC 30k 360 + MSE",
-    lalic_360_mse,
-    "LALIC 30k 360 + W-MSE + CropWidth",
-    lalic_wmse_cropwidth
-)
+# calcular_bd(
+#     "LALIC 30k 360 + MSE",
+#     lalic_360_mse,
+#     "LALIC 30k 360 + W-MSE + CropWidth",
+#     lalic_wmse_cropwidth
+# )
+
+# # wmse + 360 + random crop 256 256 vs lalic 360 + wmse + cropwidth
+
+# calcular_bd(
+#     "LALIC 30k 360 + W-MSE + RandomCrop",
+#     lalic_360_wmse_Randomcrop,
+#     "LALIC 30k 360 + W-MSE + CropWidth",
+#     lalic_wmse_cropwidth
+# )
+
+# # W-MSE + CropWidth vs LALIC original
+
+# calcular_bd(
+#     "LALIC original",
+#     lalic_original,
+#     "LALIC 30k 360 + W-MSE + CropWidth",
+#     lalic_wmse_cropwidth
+# )
 
 
-# W-MSE + CropWidth vs LALIC original
+# # Treino com imagens 360 + MSE vs treino com imagens planas
+
+# calcular_bd(
+#     "LALIC 30k planas + MSE",
+#     lalic_30k_planas,
+#     "LALIC 30k 360 + MSE",
+#     lalic_360_mse
+# )
+
+
+# # Método proposto vs treino com 30k imagens planas
+
+# calcular_bd(
+#     "LALIC 30k planas + MSE",
+#     lalic_30k_planas,
+#     "LALIC 30k 360 + W-MSE + CropWidth",
+#     lalic_wmse_cropwidth
+# )
 
 calcular_bd(
     "LALIC original",
     lalic_original,
-    "LALIC 30k 360 + W-MSE + CropWidth",
-    lalic_wmse_cropwidth
+    "30k planas + MSE",
+    lalic_30k_planas
 )
 
-
-# Treino com imagens 360 + MSE vs treino com imagens planas
-
 calcular_bd(
-    "LALIC 30k planas + MSE",
-    lalic_30k_planas,
-    "LALIC 30k 360 + MSE",
+    "LALIC original",
+    lalic_original,
+    "30k 360 + MSE",
     lalic_360_mse
 )
 
-
-# Método proposto vs treino com 30k imagens planas
+calcular_bd(
+    "LALIC original",
+    lalic_original,
+    "360 + W-MSE + Random Crop",
+    lalic_360_wmse_Randomcrop
+)
 
 calcular_bd(
-    "LALIC 30k planas + MSE",
-    lalic_30k_planas,
-    "LALIC 30k 360 + W-MSE + CropWidth",
+    "LALIC original",
+    lalic_original,
+    "360 + W-MSE + Crop Width",
     lalic_wmse_cropwidth
+)
+
+# ---------------------------------------------------------
+
+# compaaração com o original treinado em 30k planas por 20 épocas 
+
+
+calcular_bd(
+    "LALIC original treinado com 30k planas (1024x512), por 20 épocas",
+    lalic_30k_planas,
+    "30k 360 + MSE",
+    lalic_360_mse
+)
+
+calcular_bd(
+    "LALIC original treinado com 30k planas (1024x512), por 20 épocas",
+    lalic_30k_planas,
+    "360 + W-MSE + Random Crop",
+    lalic_360_wmse_Randomcrop
+)
+
+calcular_bd(
+    "LALIC original treinado com 30k planas (1024x512), por 20 épocas   ",
+    lalic_30k_planas,
+    "360 + W-MSE + Crop Width",
+    lalic_wmse_cropwidth
+)
+
+def plot_rcd_vs_anchor(nome_anchor, anchor, nome_testes):
+
+    for nome_test, test in nome_testes.items():
+
+        print("=" * 10)
+        print(f"Anchor: {nome_anchor}")
+        print(f"Test:   {nome_test}")
+        print("=" * 10)
+
+        bd.plot_rcd(
+            anchor["bpp"],
+            anchor["ws_psnr"],
+            test["bpp"],
+            test["ws_psnr"],
+            method="pchip",
+            require_matching_points=True,
+            samples=1000
+        )
+
+testes_vs_original = {
+    "30k planas + MSE": lalic_30k_planas,
+
+    "30k 360 + MSE": lalic_360_mse,
+
+    "30k 360 + W-MSE + Random Crop":
+        lalic_360_wmse_Randomcrop,
+
+    "30k 360 + W-MSE + Crop Width":
+        lalic_wmse_cropwidth
+}
+
+testes_vs_30k_planas = {
+    "30k 360 + MSE": lalic_360_mse,
+
+    "30k 360 + W-MSE + Random Crop":
+        lalic_360_wmse_Randomcrop,
+
+    "30k 360 + W-MSE + Crop Width":
+        lalic_wmse_cropwidth
+}
+
+
+
+plot_rcd_vs_anchor(
+    "LALIC original",
+    lalic_original,
+    testes_vs_original
+)
+
+plot_rcd_vs_anchor(
+    "30k planas + MSE",
+    lalic_30k_planas,
+    testes_vs_30k_planas
 )
